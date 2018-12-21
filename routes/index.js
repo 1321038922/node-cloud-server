e19015708facd8916df975f95bbb01e316bacd43
Cason <1321038922@qq.com>
2018-12-21 14:13:56 +0800

    修改一下

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 const express = require("express")
 const router = express.Router()
-
+const path = require('path')
 // const login = require('../controller/login')
 const user = require('../controller/user')
 const article = require('../controller/article')
 const category = require('../controller/category')
 
-
+router.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './index.html'))
+})
 
 router.use(article);
 router.use(user);
